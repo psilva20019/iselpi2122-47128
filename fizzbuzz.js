3d19aa065ad614af6f8d3ec504851a745f31070b
psilva20019 <72602379+psilva20019@users.noreply.github.com>
2021-10-10 15:10:47 +0000

    Primeiro TPC

---
+++ fizzbuzz.js
@@ -0,0 +1,26 @@
+/*
+for(let i = 0; i < 100; i++){
+    
+    if(i % 3 == 0 && i % 5 == 0)
+        console.log("FizzBuzz")
+    else if(i % 3 == 0)
+        console.log("Fizz")
+    else if(i % 5 == 0)
+        console.log("Buzz")    
+    else(console.log(i))
+    
+}*/
+
+let text = ""
+for (let i = 0; i <= 100; ++i){
+
+    if (i % 3 == 0)
+        text = "Fizz"
+    if (i % 5 == 0) 
+        text += "Buzz"
+    if (text == "") 
+        text += i
+
+    console.log(text)
+    text = ""
+}
