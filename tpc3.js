1f6adc929dc3992970a4c94702d78b8569fe60a7
psilva20019 <72602379+psilva20019@users.noreply.github.com>
2021-10-10 15:17:51 +0000

    Update tpc3.js

--- tpc3.js
+++ tpc3.js
@@ -29,7 +29,7 @@ const countBs = function(word){
 
 const countChar = function(word,letter){
     let count = 0
-    for (let i = 0; i< word.length;i++){
+    for (let i = 0;i < word.length;i++){
         if(word[i] === letter)count++
     }
     return count 
